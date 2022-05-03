bf466693f0d8a4ffa8b9f8c749fcee3a6ede4505
maorte <maor884@gmail.com>
2022-05-03 13:15:49 +0300

    implemented with RTK

--- src/store.js
+++ src/store.js
@@ -1,12 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './reducer';
+import { configureStore } from '@reduxjs/toolkit';
+import filtersReducer from './features/filters/filtersSlice';
+import todosReducer from './features/todos/todosSlice';
 
-const composedEnhancer = composeWithDevTools(
-	applyMiddleware(thunkMiddleware),
-	// other store enhancers if any
-);
-
-const store = createStore(rootReducer, composedEnhancer);
+const store = configureStore({
+	reducer: {
+		// Define a top-level state field named `todos`, handled by `todosReducer`
+		todos: todosReducer,
+		filters: filtersReducer,
+	},
+});
 export default store;
