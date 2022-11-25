3a574b74dbe09f3ae7429de2f0ac3f4c50150a89
maorte <maor884@gmail.com>
2022-11-25 21:31:12 +0200

    more changes

--- src/store.js
+++ src/store.js
@@ -3,10 +3,10 @@ import filtersReducer from './features/filters/filtersSlice';
 import todosReducer from './features/todos/todosSlice';
 
 const store = configureStore({
-	reducer: {
-		// Define a top-level state field named `todos`, handled by `todosReducer`
-		todos: todosReducer,
-		filters: filtersReducer,
-	},
+   reducer: {
+      // Define a top-level state field named `todos`, handled by `todosReducer`
+      todos: todosReducer,
+      filters: filtersReducer,
+   },
 });
 export default store;
